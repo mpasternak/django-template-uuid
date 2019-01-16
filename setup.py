ba0c9bc83065119f52ba5dbee15f2ef35a8776e7
Michał Pasternak <michal.dtz@gmail.com>
2019-01-16 13:30:17 +0100

    Use setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,12 @@
 
 from setuptools import setup, find_packages
 
-with open('README.rst') as readme_file:
-    readme = readme_file.read()
-
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 requirements = ['django',]
 
 setup(
     name='django_template_uuid',
     version='0.0.1',
     description="Generate UUIDs for Django templates",
-    long_description=readme + '\n\n' + history,
     author="Michał Pasternak",
     author_email='michal.dtz@gmail.com',
     url='https://github.com/mpasternak/django-template-uuid',
